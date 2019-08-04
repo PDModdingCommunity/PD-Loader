cae862c40e4f255d72a0503e11aa8f1ab93b4bf9
somewhatlurker <52014015+somewhatlurker@users.noreply.github.com>
2019-08-05 07:17:59 +1000

    minor cleanup in launcher

--- source-code/source/plugins/Launcher/ConfigOption.h
+++ source-code/source/plugins/Launcher/ConfigOption.h
@@ -230,7 +230,9 @@ public:
 		cb->Top = top;
 		cb->AutoSize = true;
 		cb->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
-		cb->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(0)), static_cast<System::Int32>(static_cast<System::Byte>(0)), static_cast<System::Int32>(static_cast<System::Byte>(0)), static_cast<System::Int32>(static_cast<System::Byte>(0)));
+
+		// hack to ensure high contrast
+		cb->BackColor = System::Drawing::Color::FromArgb(0, 0, 0, 0);
 		
 		Form^ RootForm = panel->FindForm();
 		Drawing::SizeF CurrentScaleSize = RootForm->CurrentAutoScaleDimensions;

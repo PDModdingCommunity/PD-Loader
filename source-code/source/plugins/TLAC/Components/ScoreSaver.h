aa9d43818adad56c86027c2ad74f71bbc024dde8
somewhatlurker <52014015+somewhatlurker@users.noreply.github.com>
2019-09-18 15:22:29 +1000

    score saver: add viewing of percent and score, fix exex

--- source-code/source/plugins/TLAC/Components/ScoreSaver.h
+++ source-code/source/plugins/TLAC/Components/ScoreSaver.h
@@ -2,6 +2,7 @@
 #include "EmulatorComponent.h"
 #include "../Constants.h"
 #include <windows.h>
+#include <vector>
 
 namespace TLAC::Components
 {
@@ -20,7 +21,15 @@ namespace TLAC::Components
 	private:
 		static void(__stdcall* divaInitResults)(void* cls);
 		static void hookedInitResults(void* cls);
+		static void InjectCode(void* address, const std::vector<uint8_t> data);
 
 		static WCHAR configPath[256];
+		static char selHighScore[9];
+		static char selHighPct1[4];
+		static char selHighPct2[3];
+
+		int currentPv;
+		int currentDifficulty;
+		int currentDifficultyIsEx;
 	};
 }

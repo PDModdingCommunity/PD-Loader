9308788c9062f81c944e66c461402df189f101a3
somewhatlurker <52014015+somewhatlurker@users.noreply.github.com>
2019-10-04 18:58:17 +1000

    ShaderPatch: add patch config, copy lyb's toon shader improvements

--- source-code/source/plugins/ShaderPatch/src/framework.h
+++ source-code/source/plugins/ShaderPatch/src/framework.h
@@ -3,6 +3,7 @@
 #include <windows.h>
 #include <string>
 #include <vector>
+#include <map>
 #include <regex>
 #include <stdint.h>
 #include <fstream>
@@ -13,9 +14,12 @@ struct ShaderPatchInfo {
 	std::regex dataRegex;
 	std::string dataReplace;
 	std::vector<std::string> archs;
+	std::string cfg;
 };
 
 std::vector<ShaderPatchInfo> patchesVec;
+
+std::map<std::string, std::string> configMap;
 
 #pragma pack(push, 1)
 struct MsString {
@@ -57,52 +61,6 @@ int(*NvAPI_Unload)() = NULL;
 int(*NvAPI_EnumPhysicalGPUs)(int64_t** handle, int* count) = NULL;
 int(*NvAPI_GPU_GetShortName)(int64_t* handle, char* name) = NULL;
 
-void loadGpuName()
-{
-	NvAPI_QueryInterface = (void*(*)(unsigned int offset))GetProcAddress(LoadLibrary("nvapi64.dll"), "nvapi_QueryInterface");
-
-	if (NvAPI_QueryInterface == nullptr)
-	{
-		gpuName = "Other";
-		return;
-	}
-
-	NvAPI_Initialize = (int(*)())NvAPI_QueryInterface(0x0150E828);
-	NvAPI_Unload = (int(*)()) NvAPI_QueryInterface(0xD22BDD7E);
-	NvAPI_EnumPhysicalGPUs = (int(*)(int64_t** handle, int* count))NvAPI_QueryInterface(0xE5AC921F);
-	NvAPI_GPU_GetShortName = (int(*)(int64_t* handle, char* name))NvAPI_QueryInterface(0xD988F0F3);
-
-	if (NvAPI_Initialize == nullptr ||
-		NvAPI_Unload == nullptr ||
-		NvAPI_EnumPhysicalGPUs == nullptr ||
-		NvAPI_GPU_GetShortName == nullptr)
-	{
-		gpuName = "Unknown";
-		return;
-	}
-
-	int64_t* hdlGpu[64] = { 0 };
-	int nGpu = 0;
-
-	NvAPI_Initialize();
-	NvAPI_EnumPhysicalGPUs(hdlGpu, &nGpu);
-
-	//printf("[ShaderPatch] nGpu: %d\n", nGpu);
-	if (nGpu < 1)
-	{
-		gpuName = "Unknown";
-		NvAPI_Unload();
-		return;
-	}
-
-	char nameBuf[64];
-	NvAPI_GPU_GetShortName(hdlGpu[0], nameBuf);
-	NvAPI_Unload();
-
-	gpuName = nameBuf;
-	return;
-}
-
 
 
 std::vector<std::string> SplitString(const std::string& str, const std::string& delim)
@@ -126,6 +84,36 @@ std::vector<std::string> SplitString(const std::string& str, const std::string& 
 	return tokens;
 }
 
+std::string TrimString(const std::string& str, const std::string& whitespace)
+{
+	const size_t strBegin = str.find_first_not_of(whitespace);
+
+	if (strBegin == std::string::npos)
+		return "";
+
+	const size_t strEnd = str.find_last_not_of(whitespace);
+	const size_t strRange = strEnd - strBegin + 1;
+
+	return str.substr(strBegin, strRange);
+}
+
+std::string StringReplace(const std::string& str, const std::string& srch, const std::string& repl)
+{
+	size_t pos = 0;
+	std::string outstr = str;
+
+	while (true)
+	{
+		pos = outstr.find(srch, pos);
+		if (pos == std::string::npos)
+			break;
+
+		outstr.replace(pos, srch.length(), repl);
+	}
+
+	return outstr;
+}
+
 
 std::wstring ExePath() {
 	WCHAR buffer[MAX_PATH];
@@ -142,64 +130,3 @@ std::wstring DirPath() {
 std::wstring CONFIG_FILE_STRING = DirPath() + L"\\plugins\\ShaderPatch.ini";
 LPCWSTR CONFIG_FILE = CONFIG_FILE_STRING.c_str();
 
-void LoadConfig()
-{
-	std::ifstream fileStream(CONFIG_FILE_STRING);
-
-	if (!fileStream.is_open())
-		return;
-
-	std::string line;
-
-	// check for BOM
-	std::getline(fileStream, line);
-	if (line.size() >= 3 && line.rfind("\xEF\xBB\xBF", 0) == 0)
-		fileStream.seekg(3);
-	else
-		fileStream.seekg(0);
-
-	while (std::getline(fileStream, line))
-	{
-		try {
-			if (line.size() <= 0 || line[0] == '#' || line[0] == '[' || (line.size() >= 2 && line.rfind("//", 0) == 0))
-				continue;
-
-			std::vector<std::string> equalSplit = SplitString(line, "=");
-			if (equalSplit.size() < 2) continue;
-
-			ShaderPatchInfo patch = ShaderPatchInfo();
-
-			patch.fileRegex = equalSplit[0];
-
-			std::vector<std::string> rules = SplitString(equalSplit[1], "||");
-			if (rules.size() < 1) continue; // probably will never trigger but whatever
-
-			for (std::string &rule : rules)
-			{
-				if (rule.size() >= 5 && rule.rfind("arch:", 0) == 0)
-				{
-					rule.erase(0, 5);
-					patch.archs = SplitString(rule, ",");
-				}
-				else if (rule.size() >= 5 && rule.rfind("from:", 0) == 0)
-				{
-					rule.erase(0, 5);
-					patch.dataRegex = rule;
-				}
-				else if (rule.size() >= 3 && rule.rfind("to:", 0) == 0)
-				{
-					rule.erase(0, 3);
-					patch.dataReplace = rule;
-				}
-			}
-
-			patchesVec.push_back(patch);
-		}
-		catch (int e)
-		{
-
-		}
-	}
-
-	fileStream.close();
-}

b0cefbe1d18f9167ce272aa705b154670169a5e5
somewhatlurker <52014015+somewhatlurker@users.noreply.github.com>
2019-10-20 17:35:51 +1100

    pause: move entire UI to in-engine sprites, make give up work, DrawText.h->Drawing.h, make framespeed modification robust (code's still a mess tho)

--- source-code/source/plugins/TLAC/Components/Drawing.h
+++ source-code/source/plugins/TLAC/Components/Drawing.h
@@ -65,17 +65,20 @@ namespace TLAC::Components
 	FontInfo*(*getFont)(FontInfo* fi, uint32_t id) = (FontInfo*(*)(FontInfo* fi, uint32_t id))0x140196510;
 	void(*fontSize)(FontInfo* fi, float width, float height) = (void(*)(FontInfo* fi, float width, float height))0x140199e60;
 
-	struct DrawTextParams
+	struct DrawParams
 	{
-		uint32_t textColour; // RGBA byte array?, so set as 0xAABBGGRR
-		uint32_t strokeColour; // ?? RGBA byte array?, so set as 0xAABBGGRR
+		uint32_t colour; // RGBA byte array?, so set as 0xAABBGGRR
+		uint32_t fillColour; // ?? RGBA byte array?, so set as 0xAABBGGRR
 		uint8_t unk08;
-		uint8_t padding09[0x3];
+		uint8_t unk09[0x3];
 		uint32_t unk0c;
 		uint32_t unk10;
 		uint32_t unk14;
 		uint32_t unk18;
-		uint64_t unk1c;
+		uint32_t layer; // 8 seems similar to default but higher
+		                // 0x19 is startup screen
+		                // I noticed some use different scaling
+		uint32_t unk20;
 		uint32_t unk24;
 		uint32_t unk28;
 		float xBegin; // ?
@@ -87,16 +90,17 @@ namespace TLAC::Components
 		FontInfo* font;
 		uint16_t unk50;
 
-		DrawTextParams(FontInfo* fi)
+		DrawParams(FontInfo* fi)
 		{
-			textColour = 0xffffffff;
-			strokeColour = 0xff808080;
+			colour = 0xffffffff;
+			fillColour = 0xff808080; // except it's not?
 			unk08 = 0;
 			unk0c = 0;
 			unk10 = 0;
 			unk14 = 0;
 			unk18 = 0;
-			unk1c = 0x7;
+			layer = 0x7;
+			unk20 = 0x0;
 			unk24 = 0xd;
 			unk28 = 0;
 			xBegin = 0;
@@ -108,9 +112,9 @@ namespace TLAC::Components
 			unk50 = 0x25a1;
 		}
 
-		DrawTextParams()
+		DrawParams()
 		{
-			DrawTextParams((FontInfo*)0x140eda860);
+			DrawParams((FontInfo*)0x140eda860);
 		}
 	};
 
@@ -176,20 +180,37 @@ namespace TLAC::Components
 		}
 	}; */
 
-	// flags aren't really known, but 1 == normal, 2 == right align?, 8 == centered
-	void drawText(DrawTextParams* dtParam, uint32_t flags, std::string str)
+	enum drawTextFlags : uint32_t
 	{
-		((void(*)(DrawTextParams*, uint32_t, const char*, int64_t))0x140198500)(dtParam, flags, str.c_str(), str.length());
+		DRAWTEXT_PARAGRAPH = 1, //?
+		DRAWTEXT_ALIGN_RIGHT = 2,
+		DRAWTEXT_ALIGN_CENTRE = 8,
+		DRAWTEXT_STROKE = 0x10000,
+	};
+
+	void drawText(DrawParams* dtParam, drawTextFlags flags, std::string str)
+	{
+		((void(*)(DrawParams*, uint32_t, const char*, int64_t))0x140198500)(dtParam, flags, str.c_str(), str.length());
 	}
 
-	void drawTextW(DrawTextParams* dtParam, uint32_t flags, std::wstring str)
+	void drawTextW(DrawParams* dtParam, drawTextFlags flags, std::wstring str)
 	{
 		const wchar_t* ptrs[2];
 		ptrs[0] = str.c_str();
 		ptrs[1] = (wchar_t*)((uint64_t)ptrs[0] + str.length() * 2);
 
-		((void(*)(DrawTextParams*, uint32_t, const wchar_t**))0x140198380)(dtParam, flags, &ptrs[0]);
+		((void(*)(DrawParams*, uint32_t, const wchar_t**))0x140198380)(dtParam, flags, &ptrs[0]);
 	}
-	//void(*drawTextW)(DrawTextParams* dtParam, uint32_t flags, MsStringW str) = (void(*)(DrawTextParams* dtParam, uint32_t flags, MsStringW str))0x140198380;
+	//void(*drawTextW)(DrawParams* dtParam, drawTextFlags flags, MsStringW str) = (void(*)(DrawParams* dtParam, drawTextFlags flags, MsStringW str))0x140198380;
+
+	struct RectangleBounds
+	{
+		float x;
+		float y;
+		float width;
+		float height;
+	};
+	void(*fillRectangle)(DrawParams* dtParam, RectangleBounds* rect) = (void(*)(DrawParams* dtParam, RectangleBounds* rect))0x140198d80;
+	void(*drawRectangle)(DrawParams* dtParam, RectangleBounds* rect) = (void(*)(DrawParams* dtParam, RectangleBounds* rect))0x140198320;
 #pragma pack(pop)
 }

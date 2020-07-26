3c3a2e831774aebcae07daf72fe594de10518791
somewhatlurker <52014015+somewhatlurker@users.noreply.github.com>
2020-07-26 15:12:20 +1000

    fix amd-tools, add `patch shaders.bat`

---
+++ source-code/data/amd-tools/patch shaders py.bat
@@ -0,0 +1,10 @@
+FarcPack ..\rom\shader.farc shader_unpacked
+"ARB Patcher Source\main.py" -i shader_unpacked -o shader_patched -g divaaft
+mkdir ..\mdata\MAMD
+mkdir ..\mdata\MAMD\rom
+copy info.txt ..\mdata\MAMD\
+FarcPack -c shader_patched ..\mdata\MAMD\rom\shader.farc
+del /Q shader_unpacked
+rmdir shader_unpacked
+del /Q shader_patched
+rmdir shader_patched

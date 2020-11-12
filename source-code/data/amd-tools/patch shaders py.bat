ddb25dab2d088ab08065b4c4c75c24110a3b717e
somewhatlurker <52014015+somewhatlurker@users.noreply.github.com>
2020-11-13 00:03:54 +1100

    amd-tools: consolidate patching and farc (un)packing into AFT Shader Patcher, and switch to xdelta vcdiff generation for use with new Novidia

--- source-code/data/amd-tools/patch shaders py.bat
+++ source-code/data/amd-tools/patch shaders py.bat
@@ -1,10 +1,1 @@
-FarcPack ..\rom\shader.farc shader_unpacked
-"ARB Patcher Source\main.py" -i shader_unpacked -o shader_patched -g divaaft
-mkdir ..\mdata\MAMD
-mkdir ..\mdata\MAMD\rom
-copy info.txt ..\mdata\MAMD\
-FarcPack -c shader_patched ..\mdata\MAMD\rom\shader_amd.farc
-del /Q shader_unpacked
-rmdir shader_unpacked
-del /Q shader_patched
-rmdir shader_patched
+"AFT Shader Patcher\aft_shader_patcher.py" -i shader.farc -o shader_patched.farc --xdelta
